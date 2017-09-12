77f298f7bb5e8d3460ae65b2c9f73d66dc03d440
Dave Waters <davewaters@live.ca>
2017-09-12 05:46:46 -0400

    Add files via upload

---
+++ docs/templates/edit-emailer-view-model.js
@@ -0,0 +1,24 @@
+/**
+ * Using Require.js to define a module responsible for creating a KO view model.
+ */
+define(
+    [
+        'jquery',
+        'knockout'
+    ],
+    function(
+        $,
+        ko
+    ) {
+        /**
+         * @constructor - A function constructor to create a KO view model for the events list view. This view model is responsible for all the functions 
+         * related to the corresponding events view and creates a two way data binding between the view and the view model for simplifying DOM manipulation.
+         */
+        var EditEmailerViewModel = function() {
+            // returning the EventsListViewModel
+            return this;
+        };
+
+        // returning the EditEmailerViewModel constructor
+        return EditEmailerViewModel;
+    });
